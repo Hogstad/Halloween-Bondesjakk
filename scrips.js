93a44d500b26b632af41ba09385d0876fe4ac6a8
Flemming Hogstad <flemming.hogstad@gmail.com>
2020-10-22 13:06:33 +0200

    sound

--- scrips.js
+++ scrips.js
@@ -1,78 +1,68 @@
 // JavaScript source code
 var boxes = document.getElementsByClassName("box");
 
-function sigve() {
+    function bondesjakkauto() {
 
-    document.getElementById("sigve").classList.toggle("sigve");
-
-}
-function test() {
-    3
-    document.getElementById("test2").classList.toggle("sigve1");
-
-}
-
-function bondesjakkauto() {
-
-    var time = 1500;
-    setTimeout(function () {
-
-        boxes[0].innerHTML = "&#128123"
-
+        var time = 1500;
         setTimeout(function () {
 
-            boxes[2].innerHTML = "&#129504";
+            boxes[0].innerHTML = "&#128123"
 
             setTimeout(function () {
 
-                boxes[4].innerHTML = "&#128123";
+                boxes[2].innerHTML = "&#129504";
 
                 setTimeout(function () {
 
-                    boxes[8].innerHTML = "&#129504";
+                    boxes[4].innerHTML = "&#128123";
 
                     setTimeout(function () {
 
-                        boxes[5].innerHTML = "&#128123";
+                        boxes[8].innerHTML = "&#129504";
 
                         setTimeout(function () {
 
-                            boxes[3].innerHTML = "&#129504";
+                            boxes[5].innerHTML = "&#128123";
 
                             setTimeout(function () {
 
-                                boxes[1].innerHTML = "&#128123";
+                                boxes[3].innerHTML = "&#129504";
+
                                 setTimeout(function () {
 
-                                    boxes[6].innerHTML = "&#129504";
+                                    boxes[1].innerHTML = "&#128123";
                                     setTimeout(function () {
 
-                                        boxes[7].innerHTML = "&#128123";
+                                        boxes[6].innerHTML = "&#129504";
                                         setTimeout(function () {
 
-                                            setInterval(function () {
+                                            boxes[7].innerHTML = "&#128123";
+                                            setTimeout(function () {
+                                                setInterval(function () {
+                                                    
+                                                    boxes[1].classList.toggle("box-background");
 
-                                                boxes[1].classList.toggle("box-background");
+                                                    boxes[4].classList.toggle("box-background");
 
-                                                boxes[4].classList.toggle("box-background");
+                                                    boxes[7].classList.toggle("box-background");
 
-                                                boxes[7].classList.toggle("box-background");
+                                                    document.getElementById("announce-box").classList.add("visible");
 
-                                                document.getElementById("announce-box").classList.add("visible");
+                                                }, 300);
 
-                                            }, 300);
+                                                setInterval(function () {
 
-                                            setInterval(function () {
+                                                    document.getElementById("announce-box").classList.toggle("test0");
 
-                                                document.getElementById("announce-box").classList.toggle("test0");
+                                                }, 300);
 
-                                            }, 300);
+                                                setInterval(function () {
 
-                                            setInterval(function () {
+                                                    document.getElementById("announce-box").classList.toggle("test1");
 
-                                                document.getElementById("announce-box").classList.toggle("test1");
+                                                }, 300);
 
-                                            }, 300);
+                                            }, time);
 
                                         }, time);
 
@@ -92,11 +82,9 @@ function bondesjakkauto() {
 
         }, time);
 
-    }, time);
-
-    console.log(boxes)
+        console.log(boxes)
 
 
 
-}
-setTimeout(function () { bondesjakkauto(); }, 1000);
+    }
+    setTimeout(function () {  }, 1000);
